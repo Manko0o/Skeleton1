6597a560a1aea3d44c05628d352a8326a25e19fb
Manko0o <fkobeszko01@gmail.com>
2021-02-21 21:41:36 +0000

    Added testing and class for staff management

---
+++ Testing1/tstStaff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            Assert.IsNotNull(AStaff);
+        }
+    }
+}
