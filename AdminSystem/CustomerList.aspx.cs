e1d584bd495b364db79c4a1982e563f48f40dd15
Manko0o <78808886+Manko0o@users.noreply.github.com>
2021-05-05 13:00:18 +0100

    Revert "created collection class properties (presentaion layer)"

--- AdminSystem/CustomerList.aspx.cs
+++ AdminSystem/CustomerList.aspx.cs
@@ -4,24 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (IsPostBack == false)
-        {
-            DisplayCustomers();
-        }
-    }
 
-    void DisplayCustomers()
-    {
-        clsCustomerCollection Customer = new clsCustomerCollection();
-        lstCustomer.DataSource = Customer.CustomerList;
-        lstCustomer.DataValueField = "CustomerNo";
-        lstCustomer.DataTextField = "Name";
-        lstCustomer.DataBind();
     }
 }
