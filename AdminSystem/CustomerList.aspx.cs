7152bc74e6c39cf04ed9019d3dfa0e4c41b83a13
RushilDMU <78877307+RushilDMU@users.noreply.github.com>
2021-05-05 16:05:59 +0100

    Added Collection Class Properties (Presentation Layer)

--- AdminSystem/CustomerList.aspx.cs
+++ AdminSystem/CustomerList.aspx.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (IsPostBack == false)
+        {
 
+            {
+                DisplayCustomers();
+            }
+        }
+    }
+
+    void DisplayCustomers()
+    {
+        clsCustomerCollection Customer = new clsCustomerCollection();
+        lstCustomerList.DataSource = Customer.CustomerList;
+        lstCustomerList.DataValueField = "CustomerNo";
+        lstCustomerList.DataTextField = "Name";
+        lstCustomerList.DataBind();
     }
 }
